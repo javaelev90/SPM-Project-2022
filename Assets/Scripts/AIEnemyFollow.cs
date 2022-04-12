28db5f1c225c1d173b9f6aa93dd5556b1250b315
HannaFrida <expresen.gameing@gmail.com>
2022-04-12 13:47:19 +0200

    Enemy Follow
    Enemy kan nu hitta spelaren efter den spawnats

--- Assets/Scripts/AIEnemyFollow.cs
+++ Assets/Scripts/AIEnemyFollow.cs
@@ -13,43 +13,61 @@ public class AIEnemyFollow : MonoBehaviour
     [SerializeField] float stopDist; // Distance from player where it stops
     [SerializeField] float retreatDist; // Distance when the enemy should back away from player
 
-    [SerializeField] GameObject player; 
+    private Transform player; 
     // Start is called before the first frame update
     void Start()
     {
-        player = GameObject.Find("Player");
+        StartCoroutine(Wait(5));
+        //Wait(5);
+    }
+
+    // Wait for player to spawn
+     IEnumerator Wait(float sec)
+    { while (player == null)
+        {
+            yield return new WaitForSeconds(sec);
+            player = GameObject.FindGameObjectWithTag("Player").transform;
+        }
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        // How far are we from the enemy
-        if (Vector3.Distance(transform.position, player.transform.position) > stopDist)
+        // Check if player exists in world
+        if (player != null)
         {
-            // move twoards player
-            transform.position = Vector3.MoveTowards(transform.position, player.transform.position, movementSpeed * Time.deltaTime);
-        }
-        // Is enemy near enogh to stop mving twoards player
-        else if (Vector3.Distance(transform.position, player.transform.position) < stopDist && Vector3.Distance(transform.position, player.transform.position) > retreatDist)
-        {
-            // Stop moving
-            transform.position = this.transform.position;
-        }
-        // If the enemy is to close
-        else if(Vector3.Distance(transform.position, player.transform.position) < retreatDist)
-        {
-            // Back away
-            transform.position = Vector3.MoveTowards(transform.position, player.transform.position, -movementSpeed * Time.deltaTime);
+            //player = GameObject.FindGameObjectWithTag("Player").transform;
+            // How far are we from the enemy
+            if (Vector3.Distance(transform.position, player.transform.position) > stopDist)
+            {
+                // move twoards player
+                transform.position = Vector3.MoveTowards(transform.position, player.transform.position, movementSpeed * Time.deltaTime);
+            }
+            // Is enemy near enogh to stop mving twoards player
+            else if (Vector3.Distance(transform.position, player.transform.position) < stopDist && Vector3.Distance(transform.position, player.transform.position) > retreatDist)
+            {
+                // Stop moving
+                transform.position = this.transform.position;
+            }
+            // If the enemy is to close
+            else if (Vector3.Distance(transform.position, player.transform.position) < retreatDist)
+            {
+                // Back away
+                transform.position = Vector3.MoveTowards(transform.position, player.transform.position, -movementSpeed * Time.deltaTime);
 
-        }
-        /*
-         // Solution with NavMesh
-        if (enemyNav.isOnNavMesh == false)
-        {
-            enemyNav.Warp(player.position);
+            }
+            /*
+             // Solution with NavMesh
+            if (enemyNav.isOnNavMesh == false)
+            {
+                enemyNav.Warp(player.position);
+            }
+
+            enemyNav.SetDestination(player.position);
+            */
         }
 
-        enemyNav.SetDestination(player.position);
-        */
+
     }
 }
