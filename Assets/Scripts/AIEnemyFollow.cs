2953c33e407b9c63ced71d4b9a62bdb8744b4c79
HannaFrida <expresen.gameing@gmail.com>
2022-04-12 13:16:57 +0200

    Enemy Follow Fixes
    Fixade så att den bara följer efter något med namnet Player

--- Assets/Scripts/AIEnemyFollow.cs
+++ Assets/Scripts/AIEnemyFollow.cs
@@ -13,33 +13,33 @@ public class AIEnemyFollow : MonoBehaviour
     [SerializeField] float stopDist; // Distance from player where it stops
     [SerializeField] float retreatDist; // Distance when the enemy should back away from player
 
-    [SerializeField] Transform player; 
+    [SerializeField] GameObject player; 
     // Start is called before the first frame update
     void Start()
     {
-        //player = GameObject.FindWithTag("Player").transform;
+        player = GameObject.Find("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
         // How far are we from the enemy
-        if (Vector3.Distance(transform.position, player.position) > stopDist)
+        if (Vector3.Distance(transform.position, player.transform.position) > stopDist)
         {
             // move twoards player
-            transform.position = Vector3.MoveTowards(transform.position, player.position, movementSpeed * Time.deltaTime);
+            transform.position = Vector3.MoveTowards(transform.position, player.transform.position, movementSpeed * Time.deltaTime);
         }
         // Is enemy near enogh to stop mving twoards player
-        else if (Vector3.Distance(transform.position, player.position) < stopDist && Vector3.Distance(transform.position, player.position) > retreatDist)
+        else if (Vector3.Distance(transform.position, player.transform.position) < stopDist && Vector3.Distance(transform.position, player.transform.position) > retreatDist)
         {
             // Stop moving
             transform.position = this.transform.position;
         }
         // If the enemy is to close
-        else if(Vector3.Distance(transform.position, player.position) < retreatDist)
+        else if(Vector3.Distance(transform.position, player.transform.position) < retreatDist)
         {
             // Back away
-            transform.position = Vector3.MoveTowards(transform.position, player.position, -movementSpeed * Time.deltaTime);
+            transform.position = Vector3.MoveTowards(transform.position, player.transform.position, -movementSpeed * Time.deltaTime);
 
         }
         /*
