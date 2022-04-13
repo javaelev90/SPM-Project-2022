78b0d055e19c73f846c809c5dcee10c4950d980e
HannaFrida <expresen.gameing@gmail.com>
2022-04-13 16:12:26 +0200

    Dialog Canvas Update
    Dialog canvasen deactiveras när all dialog är klar

--- Assets/Dialogue.cs
+++ Assets/Dialogue.cs
@@ -14,6 +14,7 @@ public class Dialogue : MonoBehaviour
     public bool isFirst;
     private bool isDone;
     private bool isTyping;
+    [SerializeField] private GameObject canvas;
     // Start is called before the first frame update
     void Start()
     {
@@ -62,6 +63,7 @@ public class Dialogue : MonoBehaviour
             StartCoroutine(Type());
         }else{
             gameObject.SetActive(false);
+            canvas.SetActive(false);
         }
     }
 }
